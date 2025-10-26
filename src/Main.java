b9ed45bb50419b6c3d184761308c822f76308904
Ethan McCue <ethan@mccue.dev>
2025-10-25 20:02:59 -0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,64 @@
+@interface Color {}
+@interface Point {}
+
+value record Vec3(
+        double x,
+        double y,
+        double z
+) {
+    Vec3() {
+        this(0, 0, 0);
+    }
+
+    Vec3 withX(double x) {
+        return new Vec3(x, y, z);
+    }
+
+    Vec3 withY(double y) {
+        return new Vec3(x, y, z);
+    }
+
+    Vec3 withZ(double z) {
+        return new Vec3(x, y, z);
+    }
+
+    Vec3 plus(Vec3 v) {
+        return new Vec3(
+                x + v.x, y + v.y, z + v.z
+        );
+    }
+
+    Vec3 multiply(double t) {
+        return new Vec3(
+                x * t, y * t, z * t
+        );
+    }
+
+    Vec3 divide(double t) {
+        return multiply(1 / t);
+    }
+
+    double length() {
+        return Math.sqrt(x * x + y * y + z * z);
+    }
+
+    double dotProduct(Vec3 v) {
+        return x * v.x + y * v.y + z * v.z;
+    }
+
+    Vec3 crossProduct(Vec3 v) {
+        return new Vec3(
+                y * v.z - z * v.y,
+                z * v.x - x * v.z,
+                x * v.y - y * v.x
+        );
+    }
+
+    Vec3 unitVector() {
+        return this.divide(length());
+    }
+}
+
 void main() {
     int image_width = 256;
     int image_height = 256;
