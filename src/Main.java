066830a3d31db54e659f6f50f34277c3726a30d8
Ethan McCue <ethan@mccue.dev>
2025-10-25 20:34:20 -0400

    ...

--- src/Main.java
+++ src/Main.java
@@ -31,6 +31,12 @@ value record Vec3(
         );
     }
 
+    Vec3 minus(Vec3 v) {
+        return new Vec3(
+                x - v.x, y - v.y, z - v.z
+        );
+    }
+
     Vec3 multiply(double t) {
         return new Vec3(
                 x * t, y * t, z * t
@@ -91,26 +97,65 @@ value record Ray(
     }
 }
 
+@Color Vec3 rayColor(Ray r) {
+    var unitDirection = r.direction().unitVector();
+    var a = 0.5 * (unitDirection.y + 1);
+    return new Vec3(1, 1, 1).multiply(1.0 - a)
+            .plus(new Vec3(0.5, 0.7, 1).multiply(a));
+}
+
 void main() {
-    int image_width = 256;
-    int image_height = 256;
+    // Image
+
+    var aspectRatio = 16.0 / 9.0;
+    int imageWidth = 400;
+
+    // Calculate the image height, and ensure that it's at least 1.
+    int imageHeight = (int) (imageWidth / aspectRatio);
+    imageHeight = Math.max(imageHeight, 1);
+
+    // Camera
+    var focalLength = 1.0;
+    var viewportHeight = 2.0;
+    var viewportWidth = viewportHeight * (((double) imageWidth) / imageHeight);
+    @Point Vec3 cameraCenter = new Vec3(0, 0, 0);
+
+    // Calculate the vectors across the horizontal and down the vertical viewport edges.
+    var viewportU = new Vec3(viewportWidth, 0, 0);
+    var viewportV = new Vec3(0, -viewportHeight, 0);
+
+    // Calculate the horizontal and vertical delta vectors from pixel to pixel.
+    var pixelDeltaU = viewportU.divide(imageWidth);
+    var pixelDeltaV = viewportV.divide(imageHeight);
+
+    // Calculate the location of the upper left pixel.
+    var viewportUpperLeft = cameraCenter
+            .minus(new Vec3(0, 0, focalLength))
+            .minus(viewportU.divide(2))
+            .minus(viewportV.divide(2));
+
+    var pixel00Loc = viewportUpperLeft.plus(
+            pixelDeltaU.plus(pixelDeltaV).multiply(0.5)
+    );
 
     IO.print("P3\n");
-    IO.print(image_width);
+    IO.print(imageWidth);
     IO.print(" ");
-    IO.print(image_height);
+    IO.print(imageHeight);
     IO.print("\n255\n");
 
-    for (int j = 0; j < image_height; j++) {
+    for (int j = 0; j < imageHeight; j++) {
         System.err.print("\rScanlines remaining: ");
-        System.err.print((image_height - j));
+        System.err.print((imageHeight - j));
         System.err.print(" ");
-        for (int i = 0; i < image_width; i++) {
-            @Color Vec3 pixelColor = new Vec3(
-                    ((double) i) / (image_width - 1),
-                    ((double) j) / (image_height - 1),
-                    0.0
-            );
+        for (int i = 0; i < imageWidth; i++) {
+            var pixelCenter = pixel00Loc
+                    .plus(pixelDeltaU.multiply(i))
+                    .plus(pixelDeltaV.multiply(j));
+            var rayDirection = pixelCenter.minus(cameraCenter);
+            var r = new Ray(cameraCenter, rayDirection);
+
+            @Color Vec3 pixelColor = rayColor(r);
 
             writeColor(System.out, pixelColor);
         }
